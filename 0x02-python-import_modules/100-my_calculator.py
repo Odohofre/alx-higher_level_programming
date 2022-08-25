8ff93854c844d252d80a4d0ec8a3d509a8497d8c
Odohofre <odohofrebright@gmail.com>
2022-08-25 23:44:52 +0100

    fix error in task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,14 +4,15 @@ from sys import argv, exit
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    a = int(argv[1])
-    op = argv[2]
-    b = int(argv[3])
 
     if len(argv) != 4:
         print("Usage: {} <a> <operator> <b>".format(argv[0]))
         exit(1)
     else:
+        a = int(argv[1])
+        op = argv[2]
+        b = int(argv[3])
+        
         if op == '+':
             print("{} {} {} = {}".format(a, op, b, a + b))
         elif op == '-':
