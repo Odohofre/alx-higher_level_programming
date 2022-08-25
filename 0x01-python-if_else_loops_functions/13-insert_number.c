b8ae4dd812ca3430549d177e0a61eb438afe8842
Odohofre <odohofrebright@gmail.com>
2022-08-25 16:26:11 +0100

    change spaces to tabs

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -2,39 +2,39 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *current, *new;
+	listint_t *current, *new;
 
-    current = *head;
-    if (!head)
-        return (NULL);
+	current = *head;
+	if (!head)
+		return (NULL);
 
-    new = malloc(sizeof(listint_t));
-    if (!new)
-        return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
 
-    new->n = number;
-    new->next = NULL;
+	new->n = number;
+	new->next = NULL;
 
-    if (*head == NULL)
-        *head = new;
-    else if (number < current->n)
-    {
-        new->next = current;
-        *head = new;
-    }
-    else
-    {
-        while (current->next)
-        {
-            if (number > current->next->n)
-                current = current->next;
-            else
-                break;
-        }
+	if (*head == NULL)
+		*head = new;
+	else if (number < current->n)
+	{
+		new->next = current;
+		*head = new;
+	}
+	else
+	{
+		while (current->next)
+		{
+			if (number > current->next->n)
+				current = current->next;
+			else
+				break;
+		}
 
-        new->next = current->next;
-        current->next = new;
-    }
+		new->next = current->next;
+		current->next = new;
+	}
 
-    return (new);
+	return (new);
 }
