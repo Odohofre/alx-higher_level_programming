aab87148cafd976c8939b0e1d9a131ed4913af54
Odohofre <odohofrebright@gmail.com>
2022-08-25 23:30:49 +0100

    add tasks

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from email.policy import default
+from sys import argv, exit
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    a = int(argv[1])
+    op = argv[2]
+    b = int(argv[3])
+
+    if len(argv) != 4:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        exit(1)
+    else:
+        if op == '+':
+            print("{} {} {} = {}".format(a, op, b, a + b))
+        elif op == '-':
+            print("{} {} {} = {}".format(a, op, b, a - b))
+        elif op == '*':
+            print("{} {} {} = {}".format(a, op, b, a * b))
+        elif op == '/':
+            print("{} {} {} = {}".format(a, op, b, a / b))
+        else:
+            print("Unknown operator. Available operators: +, -, *, /")
+            exit(1)
