a3d78f426e979d62f8111633bdc40a24f70eb5f4
Odohofre <odohofrebright@gmail.com>
2022-08-29 15:20:24 +0100

    fix task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,6 +4,6 @@ def no_c(my_string):
     new_str = ""
 
     for i in my_list:
-        if (i in 'C' and i in 'c'):
+        if (i in 'C' or i in 'c'):
             my_list.remove(i)
     return (new_str.join(my_list))
