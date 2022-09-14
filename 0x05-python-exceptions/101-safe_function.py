86442de2f463eacd2e9c6bc347c7b4ed94371271
Odohofre <odohofrebright@gmail.com>
2022-09-14 02:35:11 +0100

    add advance tasks

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+from sys import stderr
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except (TypeError, ZeroDivisionError, ValueError, IndexError) as e:
+        stderr.write("Exception: {}\n".format(e))
+        return None
