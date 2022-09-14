bd4d727b23e4667b6226f9dbac1e3ead4779951f
Odohofre <odohofrebright@gmail.com>
2022-09-14 05:10:17 +0100

    fix errors in task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -8,49 +8,50 @@ Can print to stdout the square using #'s
 
 
 class Square:
-    """defines a square
+    """
+    class Square definition
 
     Args:
-        size (int): size of a side in the square
+        size (int): size of a side in square
 
     Functions:
-    __init__(self, size=0, position=(0, 0))
-    size(self)
-    size(self, value)
-    position(self)
-    position(self, value)
-    area(self)
-    my_print(self)
+        __init__(self, size, position)
+        size(self)
+        size(self, value)
+        position(self)
+        position(self, value)
+        area(self)
+        my_print(self)
     """
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initializes Square
-
-        Args:
-            size (int): size of a side of the Square. Defaults to 0.
-            position (tuple): position in the Square. Defaults to (0, 0).
         """
-        self.__size = size
-        self.__position = position
+        Initializes square
+
+        Attributes:
+            size (int): defaults to 0 if none; don't use __size to call setter
+            position (int): tuple of two positive integers
+        """
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
-        """set the value of size
+        """"
+        Getter
 
-        Args:
-            value (int): set size to value size is int and >= 0
-
-        Raises:
-            ValueError: if less than zero
-            TypeError: if not an integer
-
-        Return:
-            int: size
+        Return: size
         """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        Setter
+
+        Args:
+            value: sets size to value if int and >= 0
+        """
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -60,46 +61,42 @@ class Square:
 
     @property
     def position(self):
-        """set the value of position
+        """"
+        Getter
 
-        Args:
-            value (int): set position to value if value is tuple of
-            two positive ints and >= 0
-
-        Raises:
-            TypeError: if less than than zero
-
-        Return:
-            tuple: position
+        Return: position
         """
         return self.__position
 
     @position.setter
     def position(self, value):
-        if type(value) is not tuple or len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif type(value[0]) is not int or type(value[1]) is not int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif value[0] < 0 or value[1] < 0:
+        """
+        Setter
+
+        Args:
+            value: sets position to tuple if value is tuple of 2 positive ints
+        """
+        if type(value) is not tuple or len(value) != 2 or \
+           type(value[0]) is not int or type(value[1]) is not int or \
+           value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
 
     def area(self):
-        """calculates area of a square
+        """
+        Calculates area of square
 
         Returns:
-            int: area
+            area
         """
-        return self.__size * self.__size
+        return (self.__size)**2
 
     def my_print(self):
-        """prints in stdout the square with the character #
-        """
-        if self.size == 0:
-            print()
+        if self.__size == 0:
+            print("")
         else:
-            print("\n" * self.position[1], end="")
-            print("\n".join([" " * self.position[0] +
-                             "#" * self.size
-                             for rows in range(self.size)]))
+            print("\n" * self.__position[1], end="")
+            print("\n".join([" " * self.__position[0] +
+                             "#" * self.__size
+                             for rows in range(self.__size)]))
